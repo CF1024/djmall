8f1113d3806717642cdb5dcc846167d4f799a00f
cf <chengfan0905@163.com>
2020-06-22 10:48:54 +0800

    修改redisConfig类

--- djmall_provider/djmall_cmpt_provider/src/main/java/com/dj/mall/auth/config/RedisConfig.java
+++ djmall_provider/djmall_cmpt_provider/src/main/java/com/dj/mall/auth/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.dj.mall.auth.config;
 
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,22 +11,38 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
  * @author chengf
- * @date 2020/4/17 22:15
- * Redis配置器
+ * @date 2020/6/22 10:34
+ * Redis 配置器
  */
 @Configuration
 public class RedisConfig {
 
+    /**
+     * 设置RedisTemplate
+     *
+     * @param redisConnectionFactory 连接池工厂
+     * @return
+     */
     @Bean
     public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
         RedisTemplate redisTemplate = new RedisTemplate();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        //key序列化 String
+        // 开启自动类型支持
+        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        // FastJSON序列化
+        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        // 序列化时写入类的类型
+        fastJsonRedisSerializer.getFastJsonConfig().setSerializerFeatures(SerializerFeature.WriteClassName);
+
+        // key的序列化采用StringRedisSerializer
         redisTemplate.setKeySerializer(RedisSerializer.string());
         redisTemplate.setHashKeySerializer(RedisSerializer.string());
-        //value序列化JSON
-        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(String.class);
+
+        // 值序列化JSON
         redisTemplate.setValueSerializer(fastJsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 }
